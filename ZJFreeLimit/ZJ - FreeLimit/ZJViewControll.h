732ae131b2bab7867c2e8d1a97b5933fdcaed8b3
qianfeng <qianfeng@ZJ123.local>
2016-06-08 14:46:56 +0800

    测试:加一个制图控制器，并且设置了主界面的背景颜色

---
+++ ZJFreeLimit/ZJ - FreeLimit/ZJViewControll.h
@@ -0,0 +1,13 @@
+//
+//  ZJViewControll.h
+//  ZJ - FreeLimit
+//
+//  Created by qianfeng on 16/6/8.
+//  Copyright (c) 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJViewControll : UIViewController
+
+@end
